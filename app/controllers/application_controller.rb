b0dc301ce2aaba549ce3282c1bc03c10dc43058a
Carlos Marx <carlosmarx27@gmail.com>
2022-06-23 10:54:16 -0300

    Add notificatons to comment posts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :set_notifications, if: :current_user
+
+  private
+
+  def set_notifications
+    notifications = Notification.where(recipient: current_user).newest_first.limit(9)
+    @unread       = notifications.unread
+    @read         = notifications.read
+  end
 end
